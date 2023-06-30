15a7f4a0b9723d89093b9eab9f3b5e31f1a49c54
jubayer <sheikhabujubayer@gmail.com>
2023-06-30 16:29:54 +0600

    added some fix

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -18,5 +18,31 @@ const createStudent: RequestHandler = catchAsync(
     });
   }
 );
+const createFaculy: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { faculty, ...userData } = req.body;
+    const result = await UserService.createFaculty(faculty, userData);
 
-export const UserController = { createStudent };
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    });
+  }
+);
+
+const createAdmin: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const { admin, ...userData } = req.body;
+    const result = await UserService.createAdmin(admin, userData);
+
+    sendResponse<IUser>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Admin created successfully!',
+      data: result,
+    });
+  }
+);
+export const UserController = { createStudent, createFaculy, createAdmin };
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,14 +1,26 @@
 import express from 'express';
+
 import ValidateRequest from '../../middlewares/validateRequests';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
-
 const router = express.Router();
 
 router.post(
   '/create-student',
   ValidateRequest(UserValidation.createUserZodSchema),
   UserController.createStudent
+);
+
+router.post(
+  '/create-faculty',
+  ValidateRequest(UserValidation.createFacultyZodSchema),
+  UserController.createFaculy
+);
+
+router.post(
+  '/create-admin',
+  ValidateRequest(UserValidation.createAdminZodSchema),
+  UserController.createAdmin
 );
 
 export const UserRoutes = router;
